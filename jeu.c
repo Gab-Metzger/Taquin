c21de1574854ba91fec2cf3502d64d078bb11d46
Gabriel METZGER <gab.metzger@gmail.com>
2013-11-04 21:49:37 +0100

    initJeu

--- jeu.c
+++ jeu.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef unsigned char **Jeu;
+#define MAXSIZE 4
+
+typedef unsigned char Jeu[MAXSIZE][MAXSIZE];
 
 typedef struct t_arbre
 {
@@ -30,77 +32,48 @@ void afficheJeu(Jeu jeu, int h, int l) {
 }
 
 void initTaquin(Jeu jeu, Jeu ref, int *H, int *L, char *nf) {
-	FILE *f;
-	int i;
-	char buffer[100];
-	
-	// On ouvre le fichier qui contient le jeu de référence et le jeu.
-	f = fopen(nf, "rb");
-	if(f == NULL) {
-		fprintf(stderr,"erreur ouverture %s en lecture\n",nf);
-		exit(1);
+	FILE *f = NULL;
+	int i,j,h,l,c;
+
+	f = fopen(nf,"r");
+
+	if (f == NULL)
+	{
+		fprintf(stderr, "Erreur ouverture fichier : %s en lecture\n", nf);
 	}
 
-	// On lit la première ligne
-	fgets(buffer,100,f);
+	fscanf(f,"%d %d",&h,&l);
 
-	// On cherche la valeur du jeu de référence
-	if(strncmp(buffer, "Ref",2) != 0) {
-		fprintf(stderr, "format de fichier non reconnu\n");
-		exit(2);
-	}
-	else {
-		fgets(buffer, 100, f);
-		ref = (unsigned char **)calloc(*H,sizeof(unsigned char *));
-
-		if(ref == NULL) {
-		fprintf(stderr, "echec allocation ref\n");
-		exit(3);
-		}
-
-		for (i = 0; i < *H; i=i+1)
+	// On récupère les données du jeu 
+	for (i = 0; i < h; i=i+1)
+	{
+		for (j = 0; j < l; j=j+1)
 		{
-			ref[i] = (unsigned char *)calloc(*L,sizeof(unsigned char));
-			if (ref[i] == NULL)
-			{
-				fprintf(stderr, "echec allocation ref[%d]\n",i);
-				exit(4);
-			}
-		fread(ref[i],*L,sizeof(unsigned char),f);
-		}
-
-		fgets(buffer, 100, f);
-		if(strncmp(buffer, "Jeu",3) != 0) {
-			fprintf(stderr, "format de fichier 2 non reconnu\n");
-			exit(2);
-		}
-		else {
-			fgets(buffer, 100, f);
-			jeu = (unsigned char **)calloc(*H,sizeof(unsigned char *));
-
-			if(jeu == NULL) {
-				fprintf(stderr, "echec allocation jeu\n");
-				exit(3);
-			}
-
-			for (i = 0; i < *H; i=i+1)
-			{
-				jeu[i] = (unsigned char *)calloc(*L,sizeof(unsigned char));
-				if (jeu[i] == NULL)
-				{
-					fprintf(stderr, "echec allocation jeu[%d]\n",i);
-					exit(4);
-				}
-			fread(jeu[i],*L,sizeof(unsigned char),f);
-			}
+			fscanf(f,"%d", &c);
+			jeu[i][j] = (unsigned char)c;
 		}
 	}
+
+	// On récupère les données du jeu de référence
+	for (i = 0; i < h; i=i+1)
+	{
+		for (j = 0; j < l; j=j+1)
+		{
+			fscanf(f,"%d", &c);
+			ref[i][j] = (unsigned char)c;
+		}
+	}
+
+	fclose(f);
+	*H = h;
+	*L = l;
+	
 }
 
 int main(int argc, char const *argv[])
 {
 	int h = 3, l = 3;
-	Jeu jeuRef = NULL, jeu = NULL;
+	Jeu jeuRef,jeu;
 	initTaquin(jeu,jeuRef,&h,&l,"jeu.init");
 	afficheJeu(jeu,h,l);
 	return 0;
