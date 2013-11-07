d022dfa032cb74e6a02bc074d16e8d81c2b4a9d9
Gabriel METZGER <gab.metzger@gmail.com>
2013-11-07 10:34:04 +0100

    modif + ajoutFils + estDansArbre

--- jeu.c
+++ jeu.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 #define MAXSIZE 4
+#define TROU 0
 
 typedef unsigned char Jeu[MAXSIZE][MAXSIZE];
 
@@ -16,10 +17,6 @@ typedef struct t_arbre
   struct t_arbre *fils[4];
 } Noeud, *Arbre;
 
-typedef struct coord {
-	int x;
-	int y;
-} Coordonnees;
 
 // Fonction d'affichage d'un jeu du Taquin.
 // Le 0 correspond à la case vide.
@@ -77,79 +74,152 @@ void initTaquin(Jeu jeu, Jeu ref, int *H, int *L, char *nf) {
 	
 }
 
-Coordonnees rechercherValJeu(Jeu jeu, int h, int l, char val) {
+// Recherche les coordonnées de la lettre val dans le jeu de référance.
+void rechercherValJeu(unsigned char val, Jeu ref, int h, int l, int *x, int *y) {
 	int i,j;
-	Coordonnees res;
 
 	for (i = 0; i < h; i=i+1)
 	{
 		for (j = 0; j < l; j=j+1)
 		{
-			if (jeu[i][j] == val)
+			if (ref[i][j] == val)
 			{
-				res.x = i;
-				res.y = j;
+				*x = i;
+				*y = j;
 			}
 		}
 	}
-
-	return res;
-
 }
 
-Coordonnees rechercherValJeuRef(char val) {
-	static char posRef[] = {0,0,0,1,0,2,1,0,1,1,1,2,2,0,2,1,2,2};
-	Coordonnees res;
+// Calcul la somme des distances des caractères entre le jeu et le jeu de référance.
+int distance(Jeu jeu, Jeu ref, int h, int l) {
 
-	res.x = posRef[2*(val-'A')];
-	res.y = posRef[2*(val-'A')+1];
+	int i,j,res,sommeDistance = 0,x,y;
 
-	return res;
+	for (i = 0; i < h; i=i+1)
+	{
+		for (j = 0; j < l; j=j+1)
+		{
+			if (jeu[i][j] != TROU)
+			{
+				if (jeu[i][j] != ref[i][j])
+				{
+					rechercherValJeu(jeu[i][j],ref,h,l,&x,&y);
+					res = abs(x-i) + abs(y-j);
+					sommeDistance = sommeDistance + res;
+				}
+			}
+		}
+	}
+	return sommeDistance;
 }
 
-int distance(Jeu jeu, int h, int l, char c) {
-	Coordonnees CJeu;
-	Coordonnees CJeuRef;
-	int res;
-
-	CJeu = rechercherValJeu(jeu,h,l,c);
-	CJeuRef = rechercherValJeuRef(c);
-
-	res = abs(CJeu.x-CJeuRef.x) + abs(CJeu.y-CJeuRef.y);
-
-	return res;
-}
-
+// Déplace la case en fonction du mouvement possible
 void actionJeu(Jeu jeu, int h, int l, char c, int mvt) {
-	Coordonnees pos;
-	pos = rechercherValJeu(jeu,h,l,c);
+	int x,y;
+	rechercherValJeu(c,jeu,h,l,&x,&y);
 	char tmp
 
 	switch (mvt) {
 		case 0 : 
-		tmp = jeu[pos.x][pos.y];
-		jeu[pos.x][pos.y] = jeu[pos.x-1][pos.y];
-		jeu[pos.x-1][pos.y] = tmp;
+		tmp = jeu[x][y];
+		jeu[x][y] = jeu[x-1][y];
+		jeu[x-1][y] = tmp;
 		break;
 
 		case 1 :
-		tmp = jeu[pos.x][pos.y];
-		jeu[pos.x][pos.y] = jeu[pos.x+1][pos.y];
-		jeu[pos.x+1][pos.y] = tmp;
+		tmp = jeu[x][y];
+		jeu[x][y] = jeu[x+1][y];
+		jeu[x+1][y] = tmp;
 		break;
 
 		case 2 :
-		tmp = jeu[pos.x][pos.y];
-		jeu[pos.x][pos.y] = jeu[pos.x][pos.y-1];
-		jeu[pos.x][pos.y-1] = tmp;
+		tmp = jeu[x][y];
+		jeu[x][y] = jeu[x][y-1];
+		jeu[x][y-1] = tmp;
 		break;
 
 		case 3 :
-		tmp = jeu[pos.x][pos.y];
-		jeu[pos.x][pos.y] = jeu[pos.x][pos.y+1];
-		jeu[pos.x][pos.y+1] = tmp;
+		tmp = jeu[x][y];
+		jeu[x][y] = jeu[x][y+1];
+		jeu[x][y+1] = tmp;
 		break;		
 	}
+}
+
+////////////////////////
+// Gestion de l'arbre //
+////////////////////////
+
+
+Arbre ajoutFilsArbre(Arbre a, Jeu jeu, Jeu ref, int numeroCaseFils, int h, int l) {
+	Noeud *newNoeud;
+	newNoeud = (Noeud*)malloc(sizeof(Noeud));
+	int i,j;
+
+	for (i = 0; i < h; i=i+1)
+	{
+		for (j = 0; j < l; j=j+1)
+		{
+			newNoeud->jeu[i][j] = jeu[i][j];
+		}
+	}
+	newNoeud->distance = distance(jeu,ref,h,l);
+	newNoeud->marque = 0;
+
+	// Recherche position du TROU.
+	for (i = 0; i < h; i=i+1)
+	{
+		for (j = 0; j < l; j=j+1)
+		{
+			if (jeu[i][j] == TROU)
+			{
+				newNoeud->mt = i;
+				newNoeud->nt = j;
+			}
+		}
+	}
+
+	// Création des fils NULL
+	for (i = 0; i < 4; i=i+1)
+	{
+		newNoeud->fils[i] = NULL;
+	}
+
+	// Ajout du Noeud dans l'arbre à la position du Fils défini et si arbre vide alors création d'un nouveau.
+	if (a != NULL)
+	{
+		a->fils[numeroCaseFils] = newNoeud;	
+	}
+	else {
+		a = newNoeud;
+	}	
+
+	return a;
+}
+
+// Si le jeu est déjà dans l'arbre alors res = 1, sinon res = 0;
+int estDansArbre(Arbre a, Jeu jeu, int h, int l) {
+	int res = 0,i;
+
+	if (a == NULL) {
+		res = 0;
+	}
+	else if (distance(jeu,a->jeu,h,l) == 0) {
+		res = 1;
+	}
+	else {
+		for (i = 0; i < 4; i=i+1) {
+			if (a->fils[i] != NULL) {
+				if (res != 1) {
+					if (estDansArbre(a->fils[i],jeu,h,l) == 1) {
+						res = 1;
+					}
+				}
+			}
+		}
+	}
+	return res;
 }
 
 int main(int argc, char const *argv[])
